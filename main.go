e7d6a6ffdbc5b39c783fbdbdea9f7907caca9d31
condercx <2817529759@qq.com>
2026-06-02 17:58:42 +0800

    feat: add config file parser (-conf), rewrite install script matching hy2 workflow

--- main.go
+++ main.go
@@ -1,11 +1,13 @@
 package main
 
 import (
+	"bufio"
 	"flag"
 	"fmt"
 	"net"
 	"os"
 	"strconv"
+	"strings"
 
 	qtls "github.com/metacubex/tls"
 
@@ -42,13 +44,71 @@ func main() {
 		os.Exit(1)
 	}
 
-	cert, err := qtls.LoadX509KeyPair(*certFile, *keyFile)
+	startServer(*listen, *password, *certFile, *keyFile)
+}
+
+func runWithConfig(path string) {
+	f, err := os.Open(path)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "open config: %v\n", err)
+		os.Exit(1)
+	}
+	defer f.Close()
+
+	var listen, password, certFile, keyFile string
+
+	scanner := bufio.NewScanner(f)
+	for scanner.Scan() {
+		line := strings.TrimSpace(scanner.Text())
+		if line == "" || strings.HasPrefix(line, "#") {
+			continue
+		}
+		parts := strings.SplitN(line, "=", 2)
+		if len(parts) != 2 {
+			continue
+		}
+		key := strings.TrimSpace(parts[0])
+		val := strings.TrimSpace(parts[1])
+		switch key {
+		case "listen":
+			listen = val
+		case "password":
+			password = val
+		case "cert":
+			certFile = val
+		case "key":
+			keyFile = val
+		}
+	}
+
+	if err := scanner.Err(); err != nil {
+		fmt.Fprintf(os.Stderr, "read config: %v\n", err)
+		os.Exit(1)
+	}
+
+	if listen == "" {
+		listen = ":8443"
+	}
+	if password == "" {
+		fmt.Println("error: password not set in config")
+		os.Exit(1)
+	}
+	if certFile == "" || keyFile == "" {
+		fmt.Println("error: cert and key must be set in config")
+		os.Exit(1)
+	}
+
+	startServer(listen, password, certFile, keyFile)
+}
+
+func startServer(listen, password, certFile, keyFile string) {
+	cert, err := qtls.LoadX509KeyPair(certFile, keyFile)
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "load cert: %v\n", err)
 		os.Exit(1)
 	}
 
-	host, portStr, err := net.SplitHostPort(*listen)
+	host, portStr, err := net.SplitHostPort(listen)
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "parse listen addr: %v\n", err)
 		os.Exit(1)
@@ -58,10 +118,11 @@ func main() {
 	cfg := twin.Config{
 		ServerAddr: host,
 		ServerPort: port,
-		Password:   *password,
+		Password:   password,
 	}
 	cfg.TLSCert = cert
 
+	fmt.Printf("[twin] twin server listening on %s\n", listen)
 	server := twin.NewServer(&cfg)
 	if err := server.Start(); err != nil {
 		fmt.Fprintf(os.Stderr, "start: %v\n", err)
@@ -69,11 +130,5 @@ func main() {
 	}
 
 	<-make(chan struct{})
-}
-
-func runWithConfig(path string) {
-	fmt.Printf("loading config from %s ...\n", path)
-	fmt.Println("config file support coming soon")
-	os.Exit(1)
 }
 
